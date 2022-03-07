16fc7b133c5b621329ac048527163abd72be8684
egishitakuya <100108294+egishitakuya@users.noreply.github.com>
2022-03-07 14:27:12 +0900

    Js追加機能　テキスト

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+  alert(inputText);
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  console.log(div);
+
+  // liタグ作成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // divタグの子要素に各要素を設定
+  div.appendChild(li);
+
+  // 未完了に追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
